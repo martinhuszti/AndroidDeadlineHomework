7e62b28b28290d55672e0493c7fc0ab4ba46efc7
Martin Huszti <martinhuszti@gmail.com>
2018-10-31 01:05:06 +0100

    CalendarIntent

---
+++ app/src/main/java/huszti/martin/deadline/fragments/DetailsTaskDialogFragment.kt
@@ -0,0 +1,9 @@
+package huszti.martin.deadline.fragments
+
+import android.support.v4.app.DialogFragment
+
+
+class DetailsTaskDialogFragment : DialogFragment() {
+
+
+}
--- app/src/main/java/huszti/martin/deadline/fragments/NewTaskDialogFragment.kt
+++ app/src/main/java/huszti/martin/deadline/fragments/NewTaskDialogFragment.kt
@@ -2,8 +2,11 @@ package huszti.martin.deadline.fragments
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.support.v4.app.DialogFragment
+import android.util.EventLog
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.EditText
@@ -59,21 +62,29 @@ class NewTaskDialogFragment : DialogFragment() {
         var dS = Date() //dateSelected
         dS.date = ddp!!.selectedDay.day
         dS.month = ddp!!.selectedDay.month // mert 0-tól 11ig megy
-        dS.year = ddp!!.year
+        dS.year = ddp!!.year-1900
         dS.hours = 23
-        dS.minutes = 53
-        task.dueDate = dS.toString()
+        dS.minutes = 59
+
         var today = Date()
-        today.year += 1900 // Collapsable calendar mas formatum miatt
+        today.year
 
-        task.dueDate = dS.year.toString() + "-" + dS.month.toString() + "-" + dS.date.toString()
+        task.dueDate = (dS.year+1900).toString() + "-" + dS.month.toString() + "-" + dS.date
+                .toString()
         task.remanindays = TimeUnit.MILLISECONDS.toDays(dS.time - today.time).toInt()
-
-
-
-
+        addEvent(task.title, dS.time )
         return task
+    }
 
+    fun addEvent(title: String, begin: Long) {
+        val intent = Intent(Intent.ACTION_INSERT).apply {
+            data = CalendarContract.Events.CONTENT_URI
+            putExtra(CalendarContract.Events.TITLE, title)
+            putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, begin)
+
+        }
+
+            startActivity(intent)
 
     }
 
